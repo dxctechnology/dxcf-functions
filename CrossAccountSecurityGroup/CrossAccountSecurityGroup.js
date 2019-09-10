21913d8a7e7c3d14934dbb9ac74c2487e927067d
Michael Crawford <michael.crawford@mjcconsulting.com>
2019-09-09 19:23:20 -0700

    converted to async/await pattern

--- CrossAccountSecurityGroup/CrossAccountSecurityGroup.js
+++ CrossAccountSecurityGroup/CrossAccountSecurityGroup.js
@@ -2,143 +2,96 @@
 * CrossAccountSecurityGroup: A Lambda function that returns information about a single Security Group which may be in another Account and/or Region.
 **/
 
-exports.handler = function(event, context) {
-  console.info('Request body:\n' + JSON.stringify(event));
+const response = require('cfn-response-promise');
 
-  let responseData = {};
-  let params = {};
+const AWS = require('aws-sdk');
+//AWS.config.update({region: process.env.AWS_REGION});
+AWS.config.apiVersions = {
+  sts: '2011-06-15',
+  ec2: '2016-11-15'
+};
 
-  let region = (event.ResourceProperties.Region) ? event.ResourceProperties.Region : process.env.AWS_REGION;
-
-  let accountId = (event.ResourceProperties.AccountId) ? event.ResourceProperties.AccountId : context.invokedFunctionArn.split(':')[4];
-
-  let vpcId = (event.ResourceProperties.VpcId) ? event.ResourceProperties.VpcId : '*';
-
-  let groupName = event.ResourceProperties.GroupName;
-  if (! groupName) {
-    responseData = {Error: 'GroupName missing'};
-    console.error('Error: ' + responseData.Error);
-    sendResponse(event, context, 'FAILED', responseData);
-    return;
-  }
-
-  let roleArn = 'arn:aws:iam::' + accountId + ':role/CrossAccountReadOnlyRole';
-
-  const AWS = require('aws-sdk');
-  AWS.config.update({region: region});
-  AWS.config.apiVersions = {
-    sts: '2011-06-15',
-    ec2: '2016-11-15'
-  };
-
+const assumeRole = async (roleArn, roleSessionName) => {
   const sts = new AWS.STS();
+
+  const params = {
+    RoleArn: roleArn,
+    RoleSessionName: roleSessionName
+  };
+  const data = await sts.assumeRole(params).promise();
+  //console.info(`- AssumeRole Data:\n${JSON.stringify(data, null, 2)}`);
+
+  return data.Credentials;
+};
+
+const getSecurityGroupByName = async (groupName, vpcId, credentials) => {
+  const ec2 = (credentials) ? new AWS.EC2({accessKeyId: credentials.AccessKeyId,
+                                           secretAccessKey: credentials.SecretAccessKey,
+                                           sessionToken: credentials.SessionToken})
+                            : new AWS.EC2();
+
+  const params = {
+    Filters: [{Name: 'group-name', Values: [ groupName ]},
+              {Name: 'vpc-id',     Values: [ vpcId ]}]
+  };
+  const data = await ec2.describeSecurityGroups(params).promise();
+  //console.info(`- DescribeSecurityGroups Data:\n${JSON.stringify(data, null, 2)}`);
+
+  switch (data.SecurityGroups.length) {
+    case 0:
+      throw new Error(`Could not find ${groupName} Group`);
+    case 1:
+      return data.SecurityGroups[0];
+    default:
+      throw new Error(`Found multiple ${groupName} Groups! Must specify VpcId`);
+  }
+};
+
+exports.handler = async (event, context) => {
+  console.info(`Request Body:\n${JSON.stringify(event)}`);
 
   switch (event.RequestType) {
     case 'Create':
     case 'Update':
-      console.info('Calling: AssumeRole...');
-      params = {
-        RoleArn: roleArn,
-        RoleSessionName: 'AccountInformationSession'
-      };
-      sts.assumeRole(params, function(err, data) {
-        if (err) {
-          responseData = {Error: 'AssumeRole call failed'};
-          console.error('Error: ' + responseData.Error + ':\n', err);
-          sendResponse(event, context, 'FAILED', responseData);
-        }
-        else {
-          console.info('Role: ' + roleArn + ' assumed');
-          const ec2 = new AWS.EC2({accessKeyId: data.Credentials.AccessKeyId,
-                                   secretAccessKey: data.Credentials.SecretAccessKey,
-                                   sessionToken: data.Credentials.SessionToken});
+      try {
+        const region = event.ResourceProperties.Region || process.env.AWS_REGION;
+        if (region != process.env.AWS_REGION) AWS.config.update({region: region});
 
-          console.info('Calling: DescribeSecurityGroups...');
-          params = {
-            Filters: [{Name: 'group-name', Values: [ groupName ]},
-                      {Name: 'vpc-id',     Values: [ vpcId ]}]
-          };
-          ec2.describeSecurityGroups(params, function(err, data) {
-            if (err) {
-              responseData = {Error: 'DescribeSecurityGroups call failed'};
-              console.error('Error: ' + responseData.Error + ':\n', err);
-              sendResponse(event, context, 'FAILED', responseData);
-            }
-            else {
-              if (data.SecurityGroups.length == 1) {
-                let group = data.SecurityGroups.map(g => ({VpcId: g.VpcId, GroupId: g.GroupId, GroupName: g.GroupName}))[0];
-                responseData.VpcId = group.VpcId;
-                responseData.GroupName = group.GroupName;
-                console.info('Group: ' + group.GroupName + ' (' + group.GroupId + ')');
-                sendResponse(event, context, 'SUCCESS', responseData, group.GroupId);
-              }
-              else if (data.SecurityGroups.length > 1) {
-                responseData = {Error: 'Found multiple ' + groupName + ' Groups! Must specify VpcId'};
-                console.error('Error: ' + responseData.Error);
-                sendResponse(event, context, 'FAILED', responseData);
-              }
-              else {
-                responseData = {Error: 'Could not find ' + groupName + ' Group'};
-                console.error('Error: ' + responseData.Error);
-                sendResponse(event, context, 'FAILED', responseData);
-              }
-            }
-          });
+        const accountId = event.ResourceProperties.AccountId || context.invokedFunctionArn.split(':')[4];
+
+        const vpcId = event.ResourceProperties.VpcId || '*';
+
+        const groupName = event.ResourceProperties.GroupName;
+        if (! groupName) {
+          throw new Error(`GroupName missing`);
         }
-      });
+
+        const roleName = 'CrossAccountReadOnlyRole';
+        const roleArn = `arn:aws:iam::${accountId}:role/${roleName}`;
+        const roleSessionName = 'AccountInformationSession';
+
+        console.info(`Calling: assumeRole...`);
+        const credentials = await assumeRole(roleArn, roleSessionName);
+        console.info(`Role: ${roleArn} assumed`);
+
+        console.info(`Calling: describeSecurityGroups...`);
+        const group = await getSecurityGroupByName(groupName, vpcId, credentials);
+        console.info(`Group: ${group.GroupName} (${group.GroupId})`);
+
+        const responseData = {
+          VpcId: group.VpcId,
+          GroupName: group.GroupName
+        };
+        await response.send(event, context, response.SUCCESS, responseData, group.GroupId);
+      }
+      catch (err) {
+        const responseData = {Error: `${(err.code) ? err.code : 'Error'}: ${err.message}`};
+        console.error(responseData.Error);
+        await response.send(event, context, response.FAILED, responseData);
+      }
       break;
 
     case 'Delete':
-      sendResponse(event, context, 'SUCCESS');
-      break;
-
-    default:
-      responseData = {Error: 'Unknown operation: ' + event.RequestType};
-      console.error('Error: ' + responseData.Error);
-      sendResponse(event, context, 'FAILED', responseData);
+      await response.send(event, context, response.SUCCESS);
   }
 };
-
-function sendResponse(event, context, responseStatus, responseData, physicalResourceId, noEcho) {
-  let responseBody = JSON.stringify({
-    Status: responseStatus,
-    Reason: 'See the details in CloudWatch Log Stream: ' + context.logStreamName,
-    PhysicalResourceId: physicalResourceId || context.logStreamName,
-    StackId: event.StackId,
-    RequestId: event.RequestId,
-    LogicalResourceId: event.LogicalResourceId,
-    NoEcho: noEcho || false,
-    Data: responseData
-  });
-
-  console.info('Response body:\n', responseBody);
-
-  const https = require('https');
-  const url = require('url');
-
-  let parsedUrl = url.parse(event.ResponseURL);
-  let options = {
-    hostname: parsedUrl.hostname,
-    port: 443,
-    path: parsedUrl.path,
-    method: 'PUT',
-    headers: {
-      'content-type': '',
-      'content-length': responseBody.length
-    }
-  };
-
-  let request = https.request(options, function(response) {
-    console.info('Status code: ' + response.statusCode);
-    console.info('Status message: ' + response.statusMessage);
-    context.done();
-  });
-
-  request.on('error', function(error) {
-    console.info('send(..) failed executing https.request(..): ' + error);
-    context.done();
-  });
-
-  request.write(responseBody);
-  request.end();
-}
--- CrossAccountVpc/CrossAccountVpc.js
+++ CrossAccountVpc/CrossAccountVpc.js
@@ -2,139 +2,91 @@
 * CrossAccountVpc: A Lambda function that returns information about a single VPC which may be in another Account and/or Region.
 **/
 
-exports.handler = function(event, context) {
-  console.info('Request body:\n' + JSON.stringify(event));
+const response = require('cfn-response-promise');
 
-  let responseData = {};
-  let params = {};
+const AWS = require('aws-sdk');
+AWS.config.apiVersions = {
+  sts: '2011-06-15',
+  ec2: '2016-11-15'
+};
 
-  let region = (event.ResourceProperties.Region) ? event.ResourceProperties.Region : process.env.AWS_REGION;
-
-  let accountId = (event.ResourceProperties.AccountId) ? event.ResourceProperties.AccountId : context.invokedFunctionArn.split(':')[4];
-
-  let vpcName = event.ResourceProperties.VpcName;
-  if (! vpcName) {
-    responseData = {Error: 'VpcName missing'};
-    console.error('Error: ' + responseData.Error);
-    sendResponse(event, context, 'FAILED', responseData);
-    return;
-  }
-
-  let roleArn = 'arn:aws:iam::' + accountId + ':role/CrossAccountReadOnlyRole';
-
-  const AWS = require('aws-sdk');
-  AWS.config.update({region: region});
-  AWS.config.apiVersions = {
-    sts: '2011-06-15',
-    ec2: '2016-11-15'
-  };
-
+const assumeRole = async (roleArn, roleSessionName) => {
   const sts = new AWS.STS();
+
+  const params = {
+    RoleArn: roleArn,
+    RoleSessionName: roleSessionName
+  };
+  const data = await sts.assumeRole(params).promise();
+  console.info(`- AssumeRole Data:\n${JSON.stringify(data, null, 2)}`);
+
+  return data.Credentials;
+};
+
+const getVpcByNameTag = async (vpcNameTagValue, credentials) => {
+  const ec2 = (credentials) ? new AWS.EC2({accessKeyId: credentials.AccessKeyId,
+                                           secretAccessKey: credentials.SecretAccessKey,
+                                           sessionToken: credentials.SessionToken})
+                            : new AWS.EC2();
+
+  const params = {
+    Filters: [{Name: 'tag:Name', Values: [ vpcNameTagValue ]}]
+  };
+  const data = await ec2.describeVpcs(params).promise();
+  console.info(`- DescribeVpcs Data:\n${JSON.stringify(data, null, 2)}`);
+
+  switch (data.Vpcs.length) {
+    case 0:
+      throw new Error(`Could not find ${vpcNameTagValue} VPC`);
+    case 1:
+      return data.Vpcs[0];
+    default:
+      throw new Error(`Found multiple ${vpcNameTagValue} VPCs!`);
+  }
+};
+
+exports.handler = async (event, context) => {
+  console.info(`Request Body:\n${JSON.stringify(event)}`);
 
   switch (event.RequestType) {
     case 'Create':
     case 'Update':
-      console.info('Calling: AssumeRole...');
-      params = {
-        RoleArn: roleArn,
-        RoleSessionName: 'AccountInformationSession'
-      };
-      sts.assumeRole(params, function(err, data) {
-        if (err) {
-          responseData = {Error: 'AssumeRole call failed'};
-          console.error('Error: ' + responseData.Error + ':\n', err);
-          sendResponse(event, context, 'FAILED', responseData);
-        }
-        else {
-          console.info('Role: ' + roleArn + ' assumed');
-          const ec2 = new AWS.EC2({accessKeyId: data.Credentials.AccessKeyId,
-                                   secretAccessKey: data.Credentials.SecretAccessKey,
-                                   sessionToken: data.Credentials.SessionToken});
+      try {
+        const region = event.ResourceProperties.Region || process.env.AWS_REGION;
+        if (region != process.env.AWS_REGION) AWS.config.update({region: region});
 
-          console.info('Calling: DescribeVpcs...');
-          params = {
-            Filters: [{Name: 'tag:Name', Values: [ vpcName ]}]
-          };
-          ec2.describeVpcs(params, function(err, data) {
-            if (err) {
-              responseData = {Error: 'DescribeVpcs call failed'};
-              console.error('Error: ' + responseData.Error + ':\n', err);
-              sendResponse(event, context, 'FAILED', responseData);
-            }
-            else {
-              if (data.Vpcs.length == 1) {
-                let vpc = data.Vpcs.map(v => ({VpcId: v.VpcId, CidrBlock: v.CidrBlock}))[0];
-                responseData.CidrBlock = vpc.CidrBlock;
-                console.info('VPC: ' + vpcName + ' (' + vpc.VpcId + ')');
-                sendResponse(event, context, 'SUCCESS', responseData, vpc.VpcId);
-              }
-              else if (data.Vpcs.length > 1) {
-                responseData = {Error: 'Found multiple ' + vpcName + ' VPCs!'};
-                console.error('Error: ' + responseData.Error);
-                sendResponse(event, context, 'FAILED', responseData);
-              }
-              else {
-                responseData = {Error: 'Could not find ' + vpcName + ' VPC'};
-                console.error('Error: ' + responseData.Error);
-                sendResponse(event, context, 'FAILED', responseData);
-              }
-            }
-          });
+        const accountId = event.ResourceProperties.AccountId || context.invokedFunctionArn.split(':')[4];
+
+        const vpcName = event.ResourceProperties.VpcName;
+        if (! vpcName) {
+          throw new Error(`VpcName missing`);
         }
-      });
+
+        const roleName = 'CrossAccountReadOnlyRole';
+        const roleArn = `arn:aws:iam::${accountId}:role/${roleName}`;
+        const roleSessionName = 'AccountInformationSession';
+
+        console.info(`Calling: assumeRole...`);
+        const credentials = await assumeRole(roleArn, roleSessionName);
+        console.info(`Role: ${roleArn} assumed`);
+
+        console.info(`Calling: getVpcByNameTag...`);
+        const vpc = await getVpcByNameTag(vpcName, credentials);
+        console.info(`VPC: ${vpcName} (${vpc.VpcId})`);
+
+        const responseData = {
+          CidrBlock: vpc.CidrBlock
+        };
+        await response.send(event, context, response.SUCCESS, responseData, vpc.VpcId);
+      }
+      catch (err) {
+        const responseData = {Error: `${(err.code) ? err.code : 'Error'}: ${err.message}`};
+        console.error(responseData.Error);
+        await response.send(event, context, response.FAILED, responseData);
+      }
       break;
 
     case 'Delete':
-      sendResponse(event, context, 'SUCCESS');
-      break;
-
-    default:
-      responseData = {Error: 'Unknown operation: ' + event.RequestType};
-      console.error('Error: ' + responseData.Error);
-      sendResponse(event, context, 'FAILED', responseData);
+      await response.send(event, context, response.SUCCESS);
   }
 };
-
-function sendResponse(event, context, responseStatus, responseData, physicalResourceId, noEcho) {
-  let responseBody = JSON.stringify({
-    Status: responseStatus,
-    Reason: 'See the details in CloudWatch Log Stream: ' + context.logStreamName,
-    PhysicalResourceId: physicalResourceId || context.logStreamName,
-    StackId: event.StackId,
-    RequestId: event.RequestId,
-    LogicalResourceId: event.LogicalResourceId,
-    NoEcho: noEcho || false,
-    Data: responseData
-  });
-
-  console.info('Response body:\n', responseBody);
-
-  const https = require('https');
-  const url = require('url');
-
-  let parsedUrl = url.parse(event.ResponseURL);
-  let options = {
-    hostname: parsedUrl.hostname,
-    port: 443,
-    path: parsedUrl.path,
-    method: 'PUT',
-    headers: {
-      'content-type': '',
-      'content-length': responseBody.length
-    }
-  };
-
-  let request = https.request(options, function(response) {
-    console.info('Status code: ' + response.statusCode);
-    console.info('Status message: ' + response.statusMessage);
-    context.done();
-  });
-
-  request.on('error', function(error) {
-    console.info('send(..) failed executing https.request(..): ' + error);
-    context.done();
-  });
-
-  request.write(responseBody);
-  request.end();
-}
